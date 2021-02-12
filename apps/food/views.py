c3e4a062b777edc1ce9e845341642f75c3f6d4b2
GuillaumeM92 <gemacx@gmail.com>
2021-02-13 00:12:25 +0100

    sentry test

--- apps/food/views.py
+++ apps/food/views.py
@@ -9,6 +9,9 @@ def home(request):
     """Return the home page."""
     return render(request, "food/home.html")
 
+def index(request):
+    MAIS POURQUOI EST-IL SI MECHANT ?
+    # ...
 
 def legal(request):
     """Return the home page."""
