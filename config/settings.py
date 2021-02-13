b6b50ad73e8072e5a5580982cdc27c6397b7883d
GuillaumeM92 <gemacx@gmail.com>
2021-02-13 14:06:21 +0100

    sentry test

--- config/settings.py
+++ config/settings.py
@@ -130,16 +130,6 @@ django_heroku.settings(locals())
 sentry_sdk.init(
     dsn="https://0b74ff2d74614c48946109cc17e75d66@o523221.ingest.sentry.io/5635710",
     integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
     traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
-    # By default the SDK will try to use the SENTRY_RELEASE
-    # environment variable, or infer a git commit
-    # SHA as release, however you may want to set
-    # something more human-readable.
-    # release="myapp@1.0.0",
 )
