a310bc3bf90376b64a9e04397fa984ae7cd695a0
GuillaumeM92 <gemacx@gmail.com>
2021-02-13 10:01:21 +0100

    updated inject_db script for weekly cron task

--- apps/food/management/commands/inject_db.py
+++ apps/food/management/commands/inject_db.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from apps.food.models import Product, Category
-from apps.favorites.models import Favorite
 import requests
 import django.db.utils
 
@@ -11,11 +10,13 @@ class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
         self.products_counter = 0
+        self.updated_product_counter = 0
         self.categories_counter = 0
+        self.updated_category_counter = 0
         self.clean_counter = 0
         self.autocomplete_counter = 0
         self.url = "https://fr.openfoodfacts.org/cgi/search.pl"
-        self.params = {"action": "process", "page_size": 1000, "json": True, "page": 1}
+        self.params = {"action": "process", "page_size": 100, "json": True, "page": 1}
         self.categories = []
         self.categories_url = "https://fr.openfoodfacts.org/categories&json=True"
         self.all_product_names = []
@@ -23,11 +24,10 @@ class Command(BaseCommand):
         categories = requests.get(self.categories_url).json().get("tags")
         if categories:
             for category in categories:
-                if category["products"] >= 1500:
+                if category["products"] >= 4500:
                     self.categories.append(category)
 
     def inject_products(self):
-
         for product in self.products:
             try:
                 obj, created = Product.objects.get_or_create(
@@ -44,50 +44,51 @@ class Command(BaseCommand):
                         "fr"
                     ],
                 )
-                self.products_counter += 1
+                if created:
+                    self.define_product_categories(product)
+                    self.products_counter += 1
+                else:
+                    self.updated_product_counter += 1
 
             except (KeyError, django.db.utils.IntegrityError) as error:
                 print(error)
                 pass
 
     def inject_categories(self):
-        Category.objects.all().delete()
-
         for category in self.categories:
             try:
                 obj, created = Category.objects.get_or_create(
                     name=category["name"],
                 )
-                self.categories_counter += 1
+                if created:
+                    self.categories_counter += 1
+                else:
+                    self.updated_category_counter += 1
 
             except (KeyError, django.db.utils.IntegrityError) as error:
                 print(error)
                 pass
 
-    def define_product_categories(self):
-        for product in self.products:
-            try:
-                self.product_categories = (
-                    product["categories"]
-                    .replace("' ", "'")
-                    .replace(", ", ",")
-                    .split(",")
-                )
-                self.product_name = product["product_name_fr"]
-            except KeyError as error:
-                print(error)
-                pass
+    def define_product_categories(self, product):
+        try:
+            self.product_categories = (
+                product["categories"].replace("' ", "'").replace(", ", ",").split(",")
+            )
+            self.product_name = product["product_name_fr"]
+        except KeyError as error:
+            print(error)
+            pass
 
-            for category in self.categories:
-                if category["name"] in self.product_categories:
-                    try:
-                        product = Product.objects.get(name=self.product_name)
-                        cat = Category.objects.get(name=category["name"])
-                        product.categories.add(cat)
-                    except (AttributeError, Product.DoesNotExist) as error:
-                        print(error)
-                        print(self.product_name)
-                        pass
+        for category in self.categories:
+            if category["name"] in self.product_categories:
+                try:
+                    prod = Product.objects.get(name=self.product_name)
+                    cat = Category.objects.get(name=category["name"])
+                    prod.categories.add(cat)
+                except (AttributeError, Product.DoesNotExist) as error:
+                    print(error)
+                    print(self.product_name)
+                    pass
 
     def clean_database(self):
         for product in Product.objects.all():
@@ -99,14 +100,11 @@ class Command(BaseCommand):
         parser.add_argument("pages", type=int, help="How many pages to return")
 
     def handle(self, *args, **kwargs):
-        Category.objects.all().delete()
-        Favorite.objects.all().delete()
-        Product.objects.all().delete()
         pages = kwargs["pages"]
 
         self.inject_categories()
 
-        for num in range(1, pages):
+        for num in range(0, pages):
             self.products = []
             self.params["page"] = num
             try:
@@ -122,10 +120,12 @@ class Command(BaseCommand):
                     self.products.append(product)
 
             self.inject_products()
-            self.define_product_categories()
+            # self.define_product_categories()
 
         self.clean_database()
 
         print(f"{self.products_counter} products were added to the database.")
+        print(f"{self.updated_product_counter} products were updated.")
         print(f"{self.categories_counter} categories were added to the database.")
+        print(f"{self.updated_category_counter} categories were updated.")
         print(f"{self.clean_counter} products were cleaned off the database.")
